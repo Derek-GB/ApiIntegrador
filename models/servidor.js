be0132ec8df85c41de79b516aabcfa1cadd4356b
TodV <d2tod12@gmail.com>
2025-07-31 12:53:10 -0600

    Cambios de cors

--- models/servidor.js
+++ models/servidor.js
@@ -92,7 +92,13 @@ class servidor {
   //Funciones que tiene el express y que me permite usarlas reutilizando codigo
   middlewares() {
     this.app.use(express.static("public"));
-    this.app.use(cors());
+    this.app.use(cors(
+      {
+        origin: ['http://201.197.202.42', 'http://192.168.0.10:3000'], // Permitir solicitudes solo desde estas IPs
+        methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+        allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
+      }
+    ));
     //Habilitar el parseo de los datos del body
     this.app.use(express.json());
   }

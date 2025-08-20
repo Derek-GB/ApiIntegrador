dfa698ab3109e7bd6091d9ccd197ace429345f2b
DeringSalazar <desalazarsa@est.utn.ac.cr>
2025-08-20 16:11:01 -0600

    Modificacion en validate de authController

--- Auth/auth.controller.js
+++ Auth/auth.controller.js
@@ -112,6 +112,11 @@ class AuthController {
   }
 
   async validateToken(req, res){
+    res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.set('Pragma', 'no-cache');
+    res.set('Expires', '0');
+    res.set('Surrogate-Control', 'no-store');
+
     res.json({
         success: true,
         message: 'Token válido',
